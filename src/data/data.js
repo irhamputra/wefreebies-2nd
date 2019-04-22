31561a5aea5c61bf550b8e4d81a787512a296ad2
Muhamad Irham Prasetyo <muhamad.prasetyo@enerxess.com>
2019-04-22 22:53:37 +0200

    add static data

---
+++ src/data/data.js
@@ -0,0 +1,462 @@
+export const dataList = [
+		{id: 1, title: "Photography", path: "photography", icon: "panorama"},
+		{id: 2, title: "Mockups", path: "mockups", icon: "web"},
+		{id: 3, title: "Graphics", path: "graphics", icon: "brush"},
+		{id: 4, title: "Textures", path: "textures", icon: "texture"},
+		{id: 5, title: "Fonts", path: "fonts", icon: "sort_by_alpha"},
+		{id: 6, title: "Colours", path: "colours", icon: "color_lens"},
+		{id: 7, title: "Video", path: "video", icon: "ondemand_video"},
+		{id: 9, title: "Tools", path: "tools", icon: "build"},
+];
+
+export const photographyData = [
+		{
+				id: 1,
+				title: "Pixabay",
+				description: "Over 410000 high quality photos, illustrations, and vector graphics. Free for commercial use. No attribution required.",
+				uri: "https://pixabay.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pixabay-100x100.gif"
+		},
+		{
+				id: 2,
+				title: "Pexels",
+				description: "Free high quality photos you can use everywhere. Free for commercial use. No attribution required.",
+				uri: "https://www.pexels.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pexels_02-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "StockSnap",
+				description: "High quality and high resolution images free from all copyright restrictions – no attribution required.",
+				uri: "https://stocksnap.io/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_stocksnap-100x100.gif"
+		},
+		{
+				id: 4,
+				title: "Unsplash",
+				description: "Free (do whatever you want) high-resolution stock photos. Download 10 new free stock photos every 10 days.",
+				uri: "https://unsplash.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_unsplash-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Kaboompics",
+				description: "Great place to get breathtaking Free Pictures for business or personal projects",
+				uri: "https://kaboompics.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/2016/03/makerbook_thumbnail_kaboompics-100x100.png"
+		},
+		{
+				id: 6,
+				title: "Gratisography",
+				description: "Free high-resolution pictures for personal and commercial projects.",
+				uri: "https://gratisography.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_gratisography-100x100.gif"
+		},
+		{
+				id: 7,
+				title: "ISO Republic",
+				description: "High-quality, free photos for creatives",
+				uri: "https://isorepublic.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_ISO-100x100.gif"
+		},
+		{
+				id: 8,
+				title: "Pic Jumbo",
+				description: "Totally free photos for your commercial and personal works",
+				uri: "https://picjumbo.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnails_pic_jumbo-100x100.gif"
+		},
+		{
+				id: 9,
+				title: "Negative Space",
+				description: "Brilliant selection of completely free HD stock photography",
+				uri: "https://negativespace.co/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_negative_space-100x100.gif"
+		}
+];
+
+export const coloursData = [
+		{
+				id: 1,
+				title: "Adobe Color CC",
+				description: "Over 410000 high quality photos, illustrations, and vector graphics. Free for commercial use. No attribution required.",
+				uri: "https://color.adobe.com/explore/most-popular/?time=all",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_adobe-100x100.gif"
+		},
+		{
+				id: 2,
+				title: "Colourlovers",
+				description: "Free high quality photos you can use everywhere. Free for commercial use. No attribution required.",
+				uri: "http://www.colourlovers.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_colour_lovers-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "Coolors",
+				description: "High quality and high resolution images free from all copyright restrictions – no attribution required.",
+				uri: "http://coolors.co/browser",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_coolors-100x100.gif"
+		},
+		{
+				id: 4,
+				title: "Material Palette",
+				description: "Free (do whatever you want) high-resolution stock photos. Download 10 new free stock photos every 10 days.",
+				uri: "http://design-seeds.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_material_pallette-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Design Seeds",
+				description: "Great place to get breathtaking Free Pictures for business or personal projects",
+				uri: "http://design-seeds.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_design_seeds-100x100.gif"
+		},
+		{
+				id: 6,
+				title: "Pictaculous",
+				description: "Free high-resolution pictures for personal and commercial projects.",
+				uri: "http://www.pictaculous.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pictaculous-100x100.gif"
+		}
+];
+
+export const fontsData = [
+		{
+				id: 1,
+				title: "Google Fonts",
+				description: "Hundreds of free, open-source fonts optimized for the web. Just 3 quick steps between you and a good lookin’ website.",
+				uri: "http://google.com/fonts",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_font_google-100x100.gif"
+		},
+		{
+				id: 2,
+				title: "Font Squirrel",
+				description: "Font Squirrel scours the internet in search of free, highest-quality, designer-",
+				uri: "http://www.fontsquirrel.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_font_squirrel-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "DaFont",
+				description: "Archive of freely downloadable fonts. Browse by alphabetical listing, by style, by author or by popularity.",
+				uri: "https://dafont.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_dafont-100x100.gif"
+		},
+		{
+				id: 4,
+				title: "Abstract Fonts",
+				description: "Archive of freely downloadable fonts and dingbats. Organised alphabetically, by category, by author or by popularity. Includes discussion forum.",
+				uri: "http://abstractfonts.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_abstractfonts-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Fontspace",
+				description: "Browse through collection of thousands of fonts that have been shared by designers around the world.",
+				uri: "http://abstractfonts.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/2015/05/makerbook_thumbnail_fontspace3-100x100.png"
+		}
+];
+
+export const graphicsData = [
+		{
+				id: 1,
+				title: "Vector Me",
+				description: "Vector.me features an extensive database of free vector graphics. It enables you to quickly find the vector files you need by browsing or search through the entire collection of more than 150,000 vectors.",
+				uri: "http://vector.me/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_vectorme-100x100.gif"
+		},
+		{
+				id: 2,
+				title: "Retro Vectors",
+				description: "Retro Vectors is a source of premium quality vintage vector stock files that are free for personal and commercial use. Victorian, 40’s, 50’s.",
+				uri: "http://retrovectors.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_retro_vectors-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "QVectors",
+				description: "Qvectors.net is the place to find free Vector graphics, vector icons, vector backgrounds and more.",
+				uri: "http://qvectors.net/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_qvectors-100x100.gif"
+		},
+		{
+				id: 4,
+				title: "Vector Portal",
+				description: "Free stock vectors which designers can use in commercial projects.",
+				uri: "http://www.vectorportal.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_vector_portal-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Vecteezy",
+				description: "Free Vector Art – Vecteezy is a HUGE collection of free vector art, vector graphics, illustrator brushes, Vector Wallpaper Backgrounds, Silhouettes and more!",
+				uri: "http://www.vecteezy.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_vecteezy_portal-100x100.gif"
+		},
+		{
+				id: 6,
+				title: "Freepik",
+				description: "Freepik helps you to find free vector art, icons illustrations, PSD and photos for using in websites, magazines banners, presentations …",
+				uri: "https://www.freepik.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/2015/05/makerbook_thumbnail_freepik-100x100.png"
+		}
+];
+
+export const mockupsData = [
+		{
+				id: 1,
+				title: "Pixeden",
+				description: "Pixeden is hard at work providing high-quality premium and free web resources and graphic design templates.",
+				uri: "http://www.pixeden.com/free-graphics",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pixeden-100x100.gif"
+		},
+		{
+				id: 2,
+				title: "Graphics Burger",
+				description: "Tasty design resources made with care for each pixel. Free for both personal and commercial use. Have a bite!",
+				uri: "http://graphicburger.com/mock-ups/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_graphic_burger-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "Media Loot",
+				description: "Design is easy with Medialoot. Unlimited access to fonts, icons, PSDs, templates, patterns, and much more.",
+				uri: "http://medialoot.com/free/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnails_media_loot-100x100.gif"
+		},
+		{
+				id: 4,
+				title: "Pixel Buddha",
+				description: "Mockups for personal and commercial use, including software, web, apps, templates and themes.",
+				uri: "http://pixelbuddha.net/freebies/tag/mockups",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pixel_buddha-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Perfect Pixels",
+				description: "PSD templates of unparalleled quality",
+				uri: "http://p-px.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_perfect_pixels-100x100.gif"
+		},
+		{
+				id: 6,
+				title: "Sketch App Sources (Sketch)",
+				description: "Sketch App Sources is a catalog of free resources for web ui ux designers working with Sketch 3 by Bohemian Coding.",
+				uri: "http://www.sketchappsources.com/all-free-sources.html",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_sketch_sources-100x100.png"
+		},
+		{
+				id: 7,
+				title: "Do",
+				description: "The world’s most versatile to-do app UI kit for PS & Sketch.",
+				uri: "http://www.invisionapp.com/do",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_do-100x100.gif"
+		},
+		{
+				id: 8,
+				title: "Tethr",
+				description: "The last UI kit you'll ever need",
+				uri: "http://www.invisionapp.com/tethr",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_tethr-100x100.gif"
+		},
+		{
+				id: 9,
+				title: "UI Space",
+				description: "High quality hand-crafted PSDs and AI freebies, free fonts, mockups, the latest free icons sets and other free vectorial resources.",
+				uri: "http://uispace.net/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_uispace-100x100.gif"
+		},
+		{
+				id: 10,
+				title: "Teehan Lax Tools",
+				description: "iPhone and iPad user interface elements from Teehan Lax",
+				uri: "http://teehanlax.com/tools/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_teehan_lax-100x100.gif"
+		}
+]
+
+export const texturesData = [
+		{
+				id: 1,
+				title: "Subtle Patterns",
+				description: "Free textures for your next web project",
+				uri: "http://subtlepatterns.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_subtle_patterns-100x100.gif"
+		},
+		{
+				id: 2,
+				title: "Textures",
+				description: "Free textures for 3d modeling, texturing, photoshop and game development. All images are free for personal and commercial use.",
+				uri: "http://texturer.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_texturer-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "Pattern Library",
+				description: "Patterns shared by the most talented designers out there for you to use freely in your designs.",
+				uri: "http://thepatternlibrary.com/?makerbook",
+				imageUri: "http://makerbook.net/wp-content/uploads/2015/05/makerbook_thumbnail_patern_library-100x100.png"
+		},
+		{
+				id: 4,
+				title: "Texture King",
+				description: "Free (do whatever you want) high-resolution stock photos. Download 10 new free stock photos every 10 days.",
+				uri: "http://www.textureking.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_texture_king-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Transparent Textures",
+				description: "Great place to get breathtaking Free Pictures for business or personal projects",
+				uri: "http://www.transparenttextures.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_trans_textures1-100x100.gif"
+		},
+		{
+				id: 6,
+				title: "Freebies Gallery",
+				description: "Free high-resolution pictures for personal and commercial projects.",
+				uri: "http://www.freebiesgallery.com/backgrounds/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_freebies_gallery-100x100.gif"
+		},
+		{
+				id: 7,
+				title: "Patternico",
+				description: "High-quality, free photos for creatives",
+				uri: "http://patternico.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_patternico-100x100.gif"
+		},
+		{
+				id: 8,
+				title: "Pixeden",
+				description: "Free graphic and web backgrounds to make your design project stands out with great style.",
+				uri: "http://www.pixeden.com/graphic-web-backgrounds",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pixeden-100x100.gif"
+		},
+]
+
+export const toolsData = [
+		{
+				id: 1,
+				title: "Grammarly (Copywriting)",
+				description: "Spell check and grammar check your writing when communicating online. Boost your productivity and credibility everywhere you write!",
+				uri: "http://tr.grammarly.com/aff_c?offer_id=3&aff_id=6157",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_grammarly-100x100.png"
+		},
+		{
+				id: 2,
+				title: "Hemmingway Editor (Copywriting)",
+				description: "Hemmingway App makes your writing bold and clear",
+				uri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_grammarly-100x100.png",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_hemmingway-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "Colordot (Colour Picker)",
+				description: "Fun way to explore colours with a small cursor in a vast colour space",
+				uri: "http://color.hailpixel.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_colordot-100x100.gif"
+		},
+		{
+				id: 4,
+				title: "0 to 255 (Colour Picker)",
+				description: "A simple tool that helps designer finds variations of any color",
+				uri: "http://www.0to255.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_0_to_255-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Dunnnk (Mockups Generator)",
+				description: "Upload your designs and download the final mockup within seconds",
+				uri: "https://kaboompics.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_dunnnk-100x100.gif"
+		},
+		{
+				id: 6,
+				title: "Frame (Mockups Generator)",
+				description: "Create great looking Products Mockups for free with Frame",
+				uri: "https://gratisography.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_frame-100x100.gif"
+		},
+		{
+				id: 7,
+				title: "UI Faces (Mockups Generator)",
+				description: "Find and generate sampe avatars for user interfaces",
+				uri: "http://www.uifaces.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_UI_faces-100x100.gif"
+		},
+		{
+				id: 8,
+				title: "Wordmark.it (Font Library)",
+				description: "Wordmark.it helps you choose fonts by previewing a word of your choice with the fonts installed on your computer.",
+				uri: "http://wordmark.it/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_wordmarkit-100x100.gif"
+		},
+		{
+				id: 9,
+				title: "Type Genius (Font Library)",
+				description: "Find the perfect font combo for your next project",
+				uri: "http://www.typegenius.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_type_genius-100x100.gif"
+		},
+]
+
+export const videoData = [
+		{
+				id: 1,
+				title: "Pixabay Video",
+				description: "Over 410000 high quality photos, illustrations, and vector graphics. Free for commercial use. No attribution required.",
+				uri: "https://pixabay.com/videos/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pixabay-100x100.gif"
+		},
+		{
+				id: 2,
+				title: "Pexels Video",
+				description: "Amazing, completely free high definition stock videos",
+				uri: "https://www.pexels.com/videos/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_pexels_02-100x100.gif"
+		},
+		{
+				id: 3,
+				title: "Mazwai",
+				description: "\n" +
+						"Find the very best free HD videos for your work",
+				uri: "http://mazwai.com/#/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_stocksnap-100x100.gif"
+		},
+		{
+				id: 4,
+				title: "Coverr",
+				description: "Beautiful, free videos for your homepage. 7 new videos every Monday",
+				uri: "https://coverr.co",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_unsplash-100x100.gif"
+		},
+		{
+				id: 5,
+				title: "Vimeo Free HD",
+				description: "Free HD video footage for all",
+				uri: "https://vimeo.com/groups/freehd/",
+				imageUri: "http://makerbook.net/wp-content/uploads/2016/03/makerbook_thumbnail_kaboompics-100x100.png"
+		},
+		{
+				id: 6,
+				title: "Videezy",
+				description: "HD quality and completely free to use in your projects",
+				uri: "https://www.videezy.com/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_gratisography-100x100.gif"
+		},
+		{
+				id: 7,
+				title: "Videvo",
+				description: "Completely free stock video footage and motion graphics for use in any project.",
+				uri: "https://www.videvo.net/",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnail_ISO-100x100.gif"
+		},
+		{
+				id: 8,
+				title: "Pond5",
+				description: "9,768+ completely free video footage files from Pond 5.",
+				uri: "https://www.pond5.com/de/stockvideofootage/?free=1",
+				imageUri: "http://makerbook.net/wp-content/uploads/makerbook_thumbnails_pic_jumbo-100x100.gif"
+		}
+]
